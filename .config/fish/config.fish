9db693fdff7cd415cb0217a0f7dc5b6fb18c791b
Theo Felippe <public@theocodes.com>
2022-01-08 00:19:57 +0000

    deleted old scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 # VARIABLES
+set -x DOTFILES $HOME/dotfiles
 set -x EDITOR "nvim"
 
 # ABBREVIATIONS
@@ -7,13 +8,20 @@ abbr rel exec $SHELL
 abbr lg lazygit
 abbr ll exa -al
 abbr work tmux new-session -A -s work
+abbr a asdf
 
 # ALIASES
 alias j="z"
 
 # PATH
 fish_add_path /opt/homebrew/bin
+fish_add_path $DOTFILES/bin
 
 # PROMPT
 starship init fish | source
 
+# LOAD ASDF
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# NO GREETING
+function fish_greeting; end
