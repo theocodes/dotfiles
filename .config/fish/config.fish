6ee06f96d784ec4ed51a974545b15d7976f5e099
Theo Felippe <git@theocodes.com>
2022-01-17 23:45:21 +0000

    Make fish config more platform agnostic

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
+# NO GREETING
+function fish_greeting; end
+
 # VARIABLES
 set -x DOTFILES $HOME/dotfiles
 set -x EDITOR "nvim"
@@ -17,18 +20,21 @@ abbr dev ./scripts/dev
 # ...
 
 # PATH
-fish_add_path /opt/homebrew/bin
 fish_add_path $DOTFILES/bin
-fish_add_path /opt/homebrew/opt/postgresql@12/bin
+
+# PLATFORM SPECIFIC
+switch (uname)
+  case Linux
+    source ~/.asdf/asdf.fish
+  case Darwin
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/opt/postgresql@12/bin
+end
 
 # PROMPT
 starship init fish | source
-
-# LOAD ASDF
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
-# NO GREETING
-function fish_greeting; end
 
 # LOAD JUMP
 status --is-interactive; and source (jump shell fish | psub)
