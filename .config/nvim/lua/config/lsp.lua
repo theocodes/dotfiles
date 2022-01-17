fcfa3169a8c69e5e91332ccf7b03d28d94e8e9a9
Theo Felippe <git@theocodes.com>
2022-01-17 18:05:11 +0000

    Using LSP-Installer again

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,12 +1,5 @@
 local nvim_lsp = require('lspconfig')
 
-local servers = {'solargraph'}
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-  }
-end
-
 -- gutter icons for diagnostics
 vim.fn.sign_define(
   "LspDiagnosticsSignError",
@@ -31,33 +24,30 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
 
--- local lsp_installer = require("nvim-lsp-installer")
--- special config for lsp servers
--- lsp_installer.on_server_ready(function(server) 
---   local opts = {}
--- 
---   if server.name == 'solargraph' then
---     opts.cmd = { "~/.local/share/nvim/lsp_servers/solargraph" }
---     opts.settings = {
---       solargraph = {
---         diagnostics = true
---       }
---     }
---   end
--- 
---   if server.name == 'sumneko_lua' then
---     opts.settings = {
---       Lua = {
---         diagnostics = {
---           globals = { 'vim' }
---         }
---       }
---     }
---   end
--- 
---   server:setup(opts)
--- end)
+  if server.name == 'solargraph' then
+    opts.settings = {
+      solargraph = {
+        diagnostics = true
+      }
+    }
+  end
+
+  if server.name == 'sumneko_lua' then
+    opts.settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' }
+        }
+      }
+    }
+  end
+
+  server:setup(opts)
+end)
 
 -- disable inline lsp diagnostics
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
