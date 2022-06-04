450b1d20b625c5be2ce1f98925d65f38091e2db1
Theo Felippe <git@theocodes.com>
2022-06-04 12:47:15 +0100

    Removed unnused keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -24,7 +24,7 @@ map("n", "<C-Up>", ":resize +2<cr>")
 map("n", "<C-Down>", ":resize -2<cr>")
 
 -- Explorer
--- map("n", "<C-o>", ":Lex 30<cr>")
+map("n", "<leader>op", ":Lex 30<cr>")
 
 -- Indent selection
 map("v", "<TAB>", ">gv")
@@ -46,10 +46,6 @@ map("n", ';', ':!', term_opts)
 -- Clear search highlight
 map("n", '<C-c>', ':set hlsearch!<CR>')
 
--- run current spec
--- vim.api.nvim_set_keymap('n', '<leader>mtv', ':call RunCurrentSpecFile()<CR>', { noremap = true })
--- vim.api.nvim_set_keymap('n', '<leader>mts', ':call RunNearestSpec()<CR>', { noremap = true })
-
 -- Quit buffer with q
 map("n", "q", ":q!<CR>")
 -- map("n", "q", ":bd<CR>")
@@ -61,26 +57,20 @@ map("n", "<leader>lsp", ":LspInstallInfo<CR>")
 map("n", "<C-p>", ":GFiles!<CR>")
 map("n", "<leader>pf", ":GFiles!<CR>")
 map("n", "<leader>.", ":Files!<CR>")
--- map("n", "<C-p>", "<cmd>Telescope fd<cr>")
--- map("n", "<leader>pf", "<cmd>Telescope fd<cr>")
 
 -- open recent files
--- map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
 map("n", "<leader>fr", ":History!<CR>")
 
 -- File browser
 map("n", "<leader><leader>", ":Telescope file_browser<cr>")
 
 -- Fuzzy current buffer
--- map("n", "<C-s>", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
 map("n", "<C-s>", ":BLines<CR>")
 
 -- Live grep project
--- map("n", "<leader>*", "<cmd>Telescope live_grep<cr>")
 map("n", "<leader>*", ":Ag!<CR>")
 
 -- Fuzzy buffer list
--- map("n", "<leader>bb", "<cmd>Telescope buffers show_all_buffers=true<cr>")
 map("n", "<leader>bb", ":Buffers<CR>")
 
 -- Kill current buffer
@@ -93,14 +83,8 @@ map("n", "<leader><TAB>", "<C-^>")
 map("n", "<leader>vs", ":vs<CR>")
 map("n", "<leader>hs", ":sp<CR>")
 
--- Toggle sidebar explorer
-map("n", "<leader>op", ":NERDTreeToggle<CR>")
-
 -- Preview hunk
 map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
-
-map("n", "<leader>theme", ":Telescope colorscheme<CR>")
-map("n", "<c-x>", ":Telescope commands<CR>")
 
 -- Move between buffers
 map("n", "H", "<Plug>(cokeline-focus-prev)<CR>")
@@ -116,9 +100,6 @@ map("n", "<leader>mts", ":lua _RSPEC_SPEC_SINGLE()<CR>")
 map("n", "<leader>node", ":lua _NODE_TOGGLE()<CR>")
 map("n", "<leader>nt", ":lua _JOURNAL_TOGGLE()<CR>")
 map("n", "<leader>nf", ":lua _NOTES_TOGGLE()<CR>")
-
--- Center screen on insert
--- vim.cmd("autocmd InsertEnter * norm zz")
 
 -- LSP
 map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
