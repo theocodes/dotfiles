6d9a7d8c9cee118717f60ba18d7d70b34a540c37
Theo Felippe <git@theocodes.com>
2022-08-02 22:04:39 +0100

    Dont show trouble automatically

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -143,8 +143,8 @@ return packer.startup(function(use)
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        auto_open = true,
-        auto_close = true
+        auto_open = false,
+        auto_close = false
       }
     end
   }
