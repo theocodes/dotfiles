de86b91f05e9790fd3090830a0a4dfece9829dd7
Theo Felippe <git@theocodes.com>
2022-09-19 18:28:24 +0100

    Removed emacs config

--- other/.doom.d/defaults.el
+++
@@ -1,7 +0,0 @@
-;;; $DOOMDIR/defaults.el -*- lexical-binding: t; -*-
-
-;; Disable usal macos menu (ie File, .. )
-(menu-bar-mode 0)
-
-;; don't ask for confirmation if output buffer is taken
-(setq async-shell-command-buffer 'new-buffer)
