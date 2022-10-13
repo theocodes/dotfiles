f28b6c5eceb6dd3d0364f47efb421b7fee2e69bf
Theo Felippe <git@theocodes.com>
2022-10-13 22:21:21 +0100

    Removed emacs config

--- emacs/projects.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(use-package projectile
-  :init
-  (when (file-directory-p "~/Code")
-    (setq projectile-project-search-path '("~/Code")))
-  :config
-  (projectile-global-mode +1))
--- other/.doom.d/windows.el
+++
@@ -1,7 +0,0 @@
-;;; $DOOMDIR/windows.el -*- lexical-binding: t; -*-
-
-;; Show async shell buffers as popups
-(set-popup-rule! "\\*\\(Async Shell Command\\)" :select t :size 0.4 :side 'bottom :slot 0)
-
-;; Async shell mode starts in normal mode
-(evil-set-initial-state 'shell-mode 'normal)
