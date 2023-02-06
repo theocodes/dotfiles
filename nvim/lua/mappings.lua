8a0069246b6a8d18643bbb6c74dcc0c612e9bed7
Theo Felippe <git@theocodes.com>
2023-02-06 22:06:27 +0000

    Added floating term for lazygit and rspec

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -18,3 +18,20 @@ vmap("<S-TAB>", "<gv", { desc = "De-indent selection" })
 
 xmap("J", ":move '>+1<CR>gv-gv", { desc = "Move line down" })
 xmap("K", ":move '<-2<CR>gv-gv", { desc = "Move line up" })
+
+nmap("<leader>gg", function() utils.zellij_run("lazygit", { floating = true, close_on_exit = true }) end, { desc ="Run lazygit" })
+
+-- Test runner helpers
+
+nmap("<leader>mts", function()
+  local file = vim.fn.expand('%:p')
+  local line = vim.api.nvim_eval "line('.')"
+
+  utils.zellij_run("rspec " .. file .. ":" .. line, { floating = true })
+end, { desc ="Run single rspec spec" })
+
+nmap("<leader>mtv", function()
+  local file = vim.fn.expand('%:p')
+
+  utils.zellij_run("rspec " .. file, { floating = true })
+end, { desc ="Run single rspec spec" })
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+-- Mapping
+
 local function make_mapper(mode, o)
   local parent_opts = vim.deepcopy(o)
 
@@ -14,5 +16,35 @@ local map_opts = { remap = true, silent = true }
 M.nmap = make_mapper("n", map_opts)
 M.vmap = make_mapper("v", map_opts)
 M.xmap = make_mapper("x", map_opts)
+
+-- Terminal
+
+M.zellij_run = function(cmd, o)
+  local opts = vim.deepcopy(o)
+  local base = "zellij run"
+  local flags = ""
+
+  if opts.floating then
+    flags = flags .. " -f "
+  end
+
+  if opts.close_on_exit then
+    flags = flags .. " -c "
+  end
+
+  io.popen(base .. " " .. flags .. " -- " .. cmd)
+end
+
+-- M.run_lazygit = exec_in_floating("lazygit")
+
+-- M.run_single_spec = function()
+--   local file = vim.fn.expand('%:p')
+--   local line = vim.api.nvim_eval "line('.')"
+--   -- local term = term_for_command("rspec " .. file .. ":" .. line)
+--   local cmd = "rspec" .. file .. ":" .. line
+--   local single_spec = "zellij run -f -- echo " .. cmd
+--
+--   return exec_in_floating("ls")
+-- end
 
 return M
