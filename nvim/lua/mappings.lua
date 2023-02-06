0880db59dae0a807eedb69f65471769c7b90f0ba
Theo Felippe <git@theocodes.com>
2023-02-06 00:57:09 +0000

    Updated nvim mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,14 +5,16 @@ local xmap = utils.xmap
 
 nmap("<leader>op", "<cmd>Neotree float reveal_force_cwd<cr>", { desc = "Show floating Neotree" })
 nmap("Q", "<cmd>qa<cr>", { desc = "Quit all" })
-nmap("q", "<cmd>q<cr>", { desc = "Close window" })
+nmap("q", "<cmd>bd<cr>", { desc = "Close buffer" })
 nmap("<C-x><C-e>", ":so %<CR>", { desc = "Eval current file" })
 nmap("<leader>u", ":UndotreeToggle<CR>", { desc ="Toggle undo tree" })
 nmap("<leader><TAB>", "<C-^>", { desc = "Switch to previous buffer" })
+nmap(";", ":!", { desc = "Start shell command", silent = false })
+
+nmap("<esc>", "<cmd>nohl<cr>", { desc = "Clear search selection" })
 
 vmap("<TAB>", ">gv", { desc = "Indent selection" })
 vmap("<S-TAB>", "<gv", { desc = "De-indent selection" })
 
 xmap("J", ":move '>+1<CR>gv-gv", { desc = "Move line down" })
 xmap("K", ":move '<-2<CR>gv-gv", { desc = "Move line up" })
-
