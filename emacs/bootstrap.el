3c0b811e3a31a4a231e9ef5a872f37b33afccd06
Theo Felippe <git@theocodes.com>
2023-05-01 22:27:53 +0100

    Switched to straight for dependencies

--- emacs/bootstrap.el
+++ emacs/bootstrap.el
@@ -1,17 +1,23 @@
-;; Initialize package sources
-(require 'package)
+;; Download and install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-enable-at-startup nil)
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Install use-package
+(straight-use-package 'use-package)
 
-  ;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
+; Configure use-package to use straight.el by default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
